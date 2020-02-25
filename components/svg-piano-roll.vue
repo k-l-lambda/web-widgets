5228170f94a73f9349f288dff34cc751d700c944
k-l-lambda <k-l-lambda@gmail.com>
2020-02-25 17:54:38 +0800

    component of svg-piano-roll added.

---
+++ components/svg-piano-roll.vue
@@ -0,0 +1,57 @@
+<template>
+	<g class="piano-roll-root">
+		<rect v-for="(note, i) of notations.notes" :key="i"
+			class="note"
+			:x="note.start * timeScale"
+			:y="-note.pitch * pitchScale"
+			:width="note.duration * timeScale"
+			:height="pitchScale"
+			@click="onClickNote(note)"
+			:class="{focus: note.index === focusNoteIndex, ...(note.classes || {})}"
+		/>
+	</g>
+</template>
+
+<script>
+	export default {
+		name: "svg-piano-roll",
+
+
+		props: {
+			notations: Object,
+			// ms to px
+			timeScale: {
+				type: Number,
+				default: 1e-3,
+			},
+			// pitch to px
+			pitchScale: {
+				type: Number,
+				default: 1,
+			},
+			focusNoteIndex: Number,
+		},
+
+
+		methods: {
+			onClickNote (note) {
+				this.$emit("clickNote", note);
+			},
+		},
+	};
+</script>
+
+<style scoped>
+	.note
+	{
+		cursor: pointer;
+		opacity: 0.4;
+	}
+
+	.note:hover
+	{
+		opacity: 0.9;
+		stroke: orange;
+		stroke-width: 0.08px;
+	}
+</style>
--- views/midi-roll.vue
+++ views/midi-roll.vue
@@ -1,9 +1,15 @@
 <template>
-	<svg></svg>	
+	<svg xmlns="http://www.w3.org/2000/svg">
+	</svg>	
 </template>
 
 <script>
 	export default {
 		name: "midi-roll",
+
+
+		props: {
+			midiURL: String,
+		},
 	};
 </script>
