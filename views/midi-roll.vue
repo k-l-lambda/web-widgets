719c3cf3a37fba25dda40fa920402901966e4322
k-l-lambda <k-l-lambda@gmail.com>
2020-02-26 17:50:00 +0800

    midi-roll.vue: added scales.

--- views/midi-roll.vue
+++ views/midi-roll.vue
@@ -1,6 +1,23 @@
 <template>
 	<svg xmlns="http://www.w3.org/2000/svg" :viewBox="viewBox" :height="height">
-		<SvgPianoRoll v-if="notations" :notations="notations" />
+		<SvgPianoRoll v-if="notations" :notations="notations" :timeScale="timeScale" />
+		<g class="scales" v-if="notations">
+			<line x1="0" x2="0" :y1="-notations.keyRange.low + 1" y2="-120" />
+			<line x1="0" :x2="timeScale * notations.endTime" :y1="-notations.keyRange.low + 1" :y2="-notations.keyRange.low + 1" />
+			<g class="bar" v-for="(bar, i) of notations.bars" :key="i">
+				<line v-if="bar.index === 0"
+					:x1="bar.time * timeScale" :x2="bar.time * timeScale" :y1="-notations.keyRange.low + 1" y2="-120"
+				/>
+			</g>
+			<g class="pitch-bar" v-for="pitch of pitchScales" :key="`p-${pitch}`">
+				<line x1="-.6" x2="0" :y1="-pitch + 0.5" :y2="-pitch + 0.5" />
+				<text x="-1.5" :y="-pitch + 1" >{{pitch}}</text>
+			</g>
+			<g class="time-bar" v-for="time of timeScales" :key="`t-${time}`">
+				<line :x1="time * timeScale" :x2="time * timeScale" :y1="-notations.keyRange.low + 1" :y2="-notations.keyRange.low + 1.6" />
+				<text :x="time * timeScale" :y="-notations.keyRange.low + 3">{{time * 1e-3}}s</text>
+			</g>
+		</g>
 	</svg>	
 </template>
 
@@ -21,6 +38,10 @@
 			height: {
 				type: Number,
 				default: 200,
+			},
+			timeScale: {
+				type: Number,
+				default: 1e-3,
 			},
 		},
 
@@ -44,7 +65,23 @@
 
 				const {low, high} = this.notations.keyRange;
 
-				return `0 ${-high} ${this.notations.endTime * 1e-3} ${high - low + 1}`;
+				return `-3 ${-high} ${this.notations.endTime * this.timeScale + 4} ${high - low + 4}`;
+			},
+
+
+			pitchScales () {
+				if (!this.notations)
+					return [];
+
+				return Array(9).fill().map((_, i) => i * 12).filter(p => p >= this.notations.keyRange.low);
+			},
+
+
+			timeScales () {
+				if (!this.notations)
+					return [];
+
+				return Array(Math.ceil(this.notations.endTime / 15e+3)).fill().map((_, i) => i * 15e+3);
 			},
 		},
 
@@ -75,3 +112,29 @@
 		},
 	};
 </script>
+
+<style scoped>
+	.scales line
+	{
+		stroke: black;
+		stroke-width: 0.1;
+	}
+
+	.scales text
+	{
+		font-size: 1px;
+		text-anchor: middle;
+	}
+
+	.bar line
+	{
+		stroke: black;
+		stroke-width: 0.01;
+	}
+
+	.pitch-bar line, .time-bar line
+	{
+		stroke: black;
+		stroke-width: 0.06;
+	}
+</style>
