d0ac15bf0db7f87952e83142740e8646761572e0
k-l-lambda <k-l-lambda@gmail.com>
2020-03-14 14:23:13 +0800

    midi-matcher: added link lines.

--- tests/midi-matcher/src/views/matcher.vue
+++ tests/midi-matcher/src/views/matcher.vue
@@ -4,11 +4,19 @@
 			<button @click="match" :disabled="!criterion || !sample">match</button>
 		</p>
 		<svg xmlns="http://www.w3.org/2000/svg" height="480" viewBox="-20 -20 1000 160">
-			<g transform="translate(0, 90)">
+			<g :transform="`translate(${positionC.x}, ${positionC.y})`">
 				<PinaoRoll v-if="criterion" :notations="criterion" :timeScale="timeScale" :pitchScale="1" />
 			</g>
-			<g transform="translate(0, 160)">
+			<g :transform="`translate(${positionS.x}, ${positionS.y})`">
 				<PinaoRoll v-if="sample" :notations="sample" :timeScale="timeScale" :pitchScale="1" />
+			</g>
+			<g class="links">
+				<line v-for="link of links" :key="link.s.index"
+					:x1="positionS.x + link.s.start * timeScale"
+					:x2="positionC.x + link.c.start * timeScale"
+					:y1="positionS.y - link.s.pitch"
+					:y2="positionC.y - link.c.pitch"
+				/>
 			</g>
 		</svg>
 	</div>
@@ -38,7 +46,20 @@
 		data () {
 			return {
 				timeScale: 8e-3,
+				positionC: { x: 0, y: 90 },
+				positionS: { x: 0, y: 160 },
+				path: [],
 			};
+		},
+
+
+		computed: {
+			links () {
+				return this.path.map((ci, si) => ({ ci, si })).filter(({ ci }) => ci >= 0).map(({ ci, si }) => ({
+					c: this.criterion.notes[ci],
+					s: this.sample.notes[si],
+				}));
+			},
 		},
 
 
@@ -51,8 +72,17 @@
 					Matcher.makeMatchNodes(note, this.criterion);
 
 				const navigator = await Matcher.runNavigation(this.criterion, this.sample);
-				console.log("path:", navigator.path());
+				this.path = navigator.path();
+				//console.log("path:", navigator.path());
 			},
 		},
 	};
 </script>
+
+<style scoped>
+	.links line
+	{
+		stroke: black;
+		stroke-width: 0.1px;
+	}
+</style>
